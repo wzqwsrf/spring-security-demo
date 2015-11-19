33e92b2df5ed12e2e8f28446c277928b31f2573c
zhenqing.wang <zhenqing.wang@qunar.com>
2015-11-19 19:57:13 +0800

    add datasource

--- src/main/resources/table_sql/insert_table.sql
+++ src/main/resources/table_sql/insert_table.sql
@@ -1,9 +1,7 @@
-insert into user(id,username,password,status,descn) values(1,'admin','admin',1,'管理员');
-insert into user(id,username,password,status,descn) values(2,'user','user',1,'用户');
-
+insert into users(id,username,password,status,descn) values(1,'admin','admin',1,'管理员');
+insert into users(id,username,password,status,descn) values(2,'user','user',1,'用户');
 insert into role(id,name,descn) values(1,'ROLE_ADMIN','管理员角色');
 insert into role(id,name,descn) values(2,'ROLE_USER','用户角色');
-
 insert into user_role(user_id,role_id) values(1,1);
 insert into user_role(user_id,role_id) values(1,2);
 insert into user_role(user_id,role_id) values(2,2);
